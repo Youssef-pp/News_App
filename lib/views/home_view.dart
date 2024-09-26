b797bc34f55244e2855b06495eaa57897dc87978
youssef <youssefrayan341@gmail.com>
2024-09-26 21:39:26 +0300

    first commit

---
+++ lib/views/home_view.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/widgets/categories_list_view.dart';
+
+import '../widgets/news_list_view_builder.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+        leading: const Icon(Icons.menu),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Color.fromARGB(255, 5, 0, 0),
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(child: CategoriesListView()),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 32,
+              ),
+            ),
+            NewsListViewBuilder(
+              category: 'general',
+            ),
+          ],
+        ),
+        // child: Column(
+        //   children: [
+        //     CategoriesListView(),
+        //     SizedBox(
+        //       height: 32,
+        //     ),
+        //     Expanded(child: NewsListView()),
+        //   ],
+        // ),
+      ),
+    );
+  }
+}
